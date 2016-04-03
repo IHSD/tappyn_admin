578d27426d5852f0a76b81d364f886332f938c96
Robert Wittman <rob@ihsdigital.com>
2016-04-03 15:26:16 -0400

    Initial Commit

--- application/views/payouts/index.php
+++ application/views/payouts/index.php
@@ -26,11 +26,11 @@
             <?php else: ?>
                 <table class="table table-striped responsive-utilities jambo_table bulk_action">
                   <thead>
-                    <tr class="headings">
-                        <td></td>
+                    <tr class="headings"
                         <td>ID</td>
                         <td>Status</td>
                         <td>Created</td>
+                        <td>Claimed</td>
                         <td>Contest ID</td>
                         <td>User ID</td>
                     </tr>
@@ -39,12 +39,10 @@
                   <tbody>
                         <?php foreach($payouts as $payout): ?>
                             <tr class="even pointer">
-                                <td>
-                                    <a href="<?php echo base_url().'payouts/show/'.$payout->id; ?>"><span class='fa fa-edit'></span></a>
-                                </td>
                                 <td><?php echo $payout->id; ?></td>
                                 <td><?php echo $payout->claimed == 1 ? 'Claimed' : 'Pending'; ?></td>
                                 <td><?php echo date('M d', $payout->created_at); ?></td>
+                                <td><?php echo $payout->claimed == 1 ? $payout->transfer_id : '---------'; ?></td>
                                 <td><a href="<?php echo base_url('payouts/show/'.$payout->id); ?>"><?php echo $payout->id; ?></a></td>
                                 <td><?php echo $payout->user_id; ?></td>
                             </tr>
