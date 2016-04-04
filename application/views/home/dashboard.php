f775a46fa106664528973d9f0530c566ac12038b
Robert Wittman <rob@ihsdigital.com>
2016-04-04 17:18:20 -0400

    Added users and submissions by hour and day

--- application/views/home/dashboard.php
+++ application/views/home/dashboard.php
@@ -635,6 +635,7 @@
                     </div>
                 </div>
             </div>
+        </div>
 
             <div class='row'>
                 <div class="col-md-4 col-sm-4 col-xs-12">
@@ -824,7 +825,7 @@ $.ajax({
                 }
             }],
             series: [{
-                name: 'SignUps',
+                name: 'Submissions',
                 type: 'bar',
                 data: response.data
             }]
