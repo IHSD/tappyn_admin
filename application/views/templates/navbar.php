1b32f7f92068c8decafe1c7f8eeabe1e5b2136cb
Robert Wittman <rob@ihsdigital.com>
2016-04-04 16:20:39 -0400

    Added submission data

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -43,15 +43,7 @@
    <script src="<?php echo base_url('assets/js/input_mask/jquery.inputmask.js'); ?>"></script>
    <script src="<?php echo base_url('assets/js/chartjs/chart.min.js'); ?>"></script>
    <script src="<?php echo base_url('assets/js/datepicker/bootstrap-datepicker.js'); ?>"></script>
-  <!--[if lt IE 9]>
-            <script src="../<?php echo base_url(); ?>'assets/js/ie8-responsive-file-warning.js"></script>
-            <![endif]-->
 
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
-              <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-              <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-            <![endif]-->
 
 </head>
 
