197e618b4be44835d334011ac94867990e8a26c6
Robert Wittman <rob@ihsdigital.com>
2016-05-25 12:51:15 -0400

    Debugging

--- application/views/submissions/confirm_delete.php
+++ application/views/submissions/confirm_delete.php
@@ -23,6 +23,35 @@
           </div>
           <div class="x_content" style='overflow:auto'>
               <h4>Are you sure you would like to delete this submission?</h4>
+              <div class='submission_container col-sm-6 col-sm-offset-3'>
+                  <div class='col-sm-4'>
+                      <blockquote class="message" style='font-size:0.9em'>
+                          <?php if(!is_null($submission->headline)): ?>
+                              <p class='title'><strong>Headline</strong></p>
+                              <p><?php echo $submission->headline; ?></p>
+                          <?php endif; ?>
+                          <?php if(!is_null($submission->text)): ?>
+                              <p class='title'><strong>Text</strong></p>
+                              <p><?php echo $submission->text; ?></p>
+                          <?php endif; ?>
+                          <?php if(!is_null($submission->description)): ?>
+                              <p class='title'><strong>Description</strong></p>
+                              <p><?php echo $submission->description; ?></p>
+                          <?php endif; ?>
+                          <?php if(!is_null($submission->link_explanation)): ?>
+                              <p class='title'><strong>Link Explanation</strong></p>
+                              <p><?php echo $submission->link_explanation; ?></p>
+                          <?php endif; ?>
+                      </blockquote>
+                  </div>
+                  <div class='col-sm-4'>
+                      <?php if($contest->platform == 'instagram'): ?>
+                          <img src="<?php echo $submission->attachment; ?>">
+                      <?php else: ?>
+                          <img src="<?php echo $submission->thumbnail_url; ?>">
+                      <?php endif; ?>
+                  </div>
+              </div>
               <?php echo form_open('contests/'.$submission->contest_id.'/submissions/'.$submission->id); ?>
                   <button type='submit' class='btn btn-danger'>Confirm</button>
                   <a href="<?php echo base_url('contests/show/'.$submission->contest_id); ?>" class='btn btn-default'>Cancel</a>
