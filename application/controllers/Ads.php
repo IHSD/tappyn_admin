68b03f954bfaa44012fc145c219275482db42eca
Onion Lian <onininon@dejavu.studio>
2016-07-22 15:08:57 +0800

    fix

--- application/controllers/Ads.php
+++ application/controllers/Ads.php
@@ -40,12 +40,12 @@ class Ads extends MY_Controller
                 foreach ($post['csv_data_array'] as $row) {
                     if ($row['Title'] && $row['CTR (All)']) {
                         $submission = $this->submission_library->get_by_headline($row['Title']);
-                        var_dump($submission);
                         if (!$submission) {
                             $data['not_found'][] = $row['Title'];
                         } else {
+                            $s    = $submission[0];
                             $temp = array(
-                                'sid'             => '',
+                                'sid'             => $s->id,
                                 'ctr'             => number_format(round($row['CTR (All)'], 2), 2),
                                 'impressions'     => $row['Impressions'],
                                 'cost_per_result' => number_format(round($row['Cost per Result (USD)'], 2), 2),
